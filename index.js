1319f3349a2269d75672cf0f15809bcf0e80cfd9
Abdallah Akrab <37980706+abdallahakrab@users.noreply.github.com>
2021-03-27 04:01:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const morgan = require("morgan);
+
+morgan.token("req-body", function (req, res) {
+      return Object.keys(req.body).length !== 0 ? JSON.stringify(req.body) : "";
+  
+  
+  
+  
+  
+  
+//  usage
+  
+// Assumptions 
+  // json middleware
